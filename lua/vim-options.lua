700f4d73456bb2f26b70fe340c12106f9711e99b
vlad polewoi <vlad.polewoi1@gmail.com>
2024-04-06 18:51:13 +0300

    Add comments + save on leader+w

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,7 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set foldcolumn=2")
 vim.g.mapleader = " "
 
 vim.opt.clipboard = "unnamedplus"
 
+-- mappings
+vim.keymap.set("n", "<leader>w", ":w<CR>")
