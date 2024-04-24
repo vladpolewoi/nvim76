2c3077ceb3f0ead0adacdd975f5b99b69b4aeb99
vlad polewoi <vlad.polewoi1@gmail.com>
2024-04-25 00:00:55 +0300

    feat: Add conventional commits to build

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,39 @@
 return {
-  {  "nvim-telescope/telescope.nvim",
-    tag = '0.1.6',
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require('telescope.builtin')
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
 
-      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-    end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"olacin/telescope-cc.nvim",
+		config = function()
+			-- require("telescope").load_extension("cc")
+			require("telescope").load_extension("conventional_commits")
+			vim.keymap.set(
+				"n",
+				"<leader>c",
+				require("telescope").extensions.conventional_commits.conventional_commits,
+				{}
+			)
+		end,
+	},
 }
